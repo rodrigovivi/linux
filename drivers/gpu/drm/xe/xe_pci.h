941919526bc18e504bfbefbc17ddfd0587b71853
Jason Ekstrand <jason@jlekstrand.net>
2021-08-12 00:32:30 -0500

    Actually init the PCI device

---
+++ drivers/gpu/drm/xe/xe_pci.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright © 2021 Intel Corporation
+ */
+
+#ifndef _XE_PCI_H_
+#define _XE_PCI_H_
+
+int i915_register_pci_driver(void);
+void i915_unregister_pci_driver(void);
+
+#endif /* _XE_PCI_H_ */
