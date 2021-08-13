9700ec41f9df8a30b72001c00a6dc9639276485b
Jason Ekstrand <jason@jlekstrand.net>
2021-08-13 10:21:56 -0500

    Add some nifty BO locking helpers

--- drivers/gpu/drm/xe/xe_bo.h
+++ drivers/gpu/drm/xe/xe_bo.h
@@ -39,6 +39,38 @@ static inline void xe_bo_put(struct xe_bo *bo)
 	ttm_bo_put(&bo->ttm);
 }
 
+#define xe_bo_assert_held(bo) dma_resv_assert_held((bo)->ttm.base.resv)
+
+static inline void xe_bo_lock_vm_held(struct xe_bo *bo, struct ww_acquire_ctx *ctx)
+{
+	XE_BUG_ON(bo->vm && bo->ttm.base.resv != &bo->vm->resv);
+	if (bo->vm)
+		xe_vm_assert_held(bo->vm);
+	else
+		dma_resv_lock(bo->ttm.base.resv, ctx);
+}
+
+static inline void xe_bo_unlock_vm_held(struct xe_bo *bo)
+{
+	XE_BUG_ON(bo->vm && bo->ttm.base.resv != &bo->vm->resv);
+	if (bo->vm)
+		xe_vm_assert_held(bo->vm);
+	else
+		dma_resv_unlock(bo->ttm.base.resv);
+}
+
+static inline void xe_bo_or_vm_lock(struct xe_bo *bo, struct ww_acquire_ctx *ctx)
+{
+	XE_BUG_ON(bo->vm && bo->ttm.base.resv != &bo->vm->resv);
+	dma_resv_lock(bo->ttm.base.resv, ctx);
+}
+
+static inline void xe_bo_or_vm_unlock(struct xe_bo *bo)
+{
+	XE_BUG_ON(bo->vm && bo->ttm.base.resv != &bo->vm->resv);
+	dma_resv_unlock(bo->ttm.base.resv);
+}
+
 extern struct ttm_device_funcs xe_ttm_funcs;
 
 int xe_gem_create_ioctl(struct drm_device *dev, void *data,
