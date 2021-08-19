fcdb397050768cd6f772d09b393eb7229d57a5b6
Jason Ekstrand <jason@jlekstrand.net>
2021-08-18 19:12:12 -0500

    MOAR

---
+++ drivers/gpu/drm/xe/xe_mmio.h
@@ -0,0 +1,26 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright © 2021 Intel Corporation
+ */
+
+#ifndef _XE_MMIO_H_
+#define _XE_MMIO_H_
+
+#include "xe_device.h"
+
+int xe_mmio_init(struct xe_device *xe);
+void xe_mmio_finish(struct xe_device *xe);
+
+static inline void xe_mmio_write32(struct xe_device *xe,
+				   uint32_t reg, uint32_t val)
+{
+	writel(val, xe->mmio.regs + reg);
+}
+
+static inline uint32_t xe_mmio_read32(struct xe_device *xe, uint32_t reg)
+{
+	return readl(xe->mmio.regs + reg);
+}
+
+#endif /* _XE_MMIO_H_ */
