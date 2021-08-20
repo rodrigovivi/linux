07c54325bdd03e3af941c8ca55d8d68494ffa3a6
Jason Ekstrand <jason@jlekstrand.net>
2021-08-20 15:29:07 -0500

    Combine LRC and ring
    Aparently, GuC has rings just like execlists so there's no reason to
    stick the ring in the execlist struct.

--- drivers/gpu/drm/xe/xe_execlist.c
+++ drivers/gpu/drm/xe/xe_execlist.c
@@ -12,6 +12,7 @@
 #include "xe_sched_job.h"
 
 #include "../i915/i915_reg.h"
+#include "../i915/gt/intel_gpu_commands.h"
 #include "../i915/gt/intel_lrc_reg.h"
 
 #define XE_EXECLIST_HANG_LIMIT 1
@@ -38,82 +39,11 @@ void xe_execlist_port_destroy(struct xe_execlist_port *port)
 	kfree(port);
 }
 
-//static void xe_execlist_port_wake_locked(struct xe_execlist_port *port)
-//{
-//}
-
-static int xe_ring_init(struct xe_ring *r, struct xe_device *xe,
-			struct xe_vm *vm, struct xe_lrc *lrc, uint32_t size)
-{
-	uint32_t *lrc_regs;
-	int err;
-
-	r->lrc = lrc;
-	r->size = size;
-	r->tail = 0;
-	r->bo = xe_bo_create(xe, vm, size, ttm_bo_type_kernel,
-			     XE_BO_CREATE_SYSTEM_BIT | XE_BO_CREATE_GGTT_BIT);
-	if (IS_ERR(r->bo))
-		return PTR_ERR(r->bo);
-
-	XE_BUG_ON(size % PAGE_SIZE);
-	err = ttm_bo_kmap(&r->bo->ttm, 0, size / PAGE_SIZE, &r->kmap);
-	if (err)
-		goto err_bo;
-
-	lrc_regs = xe_lrc_regs(lrc);
-	lrc_regs[CTX_RING_START] = xe_bo_ggtt_addr(r->bo);
-	lrc_regs[CTX_RING_HEAD] = 0;
-	lrc_regs[CTX_RING_TAIL] = r->tail;
-	lrc_regs[CTX_RING_CTL] = RING_CTL_SIZE(r->size) | RING_VALID;
-
-	return 0;
-
-err_bo:
-	xe_bo_put(r->bo);
-	return err;
-}
-
-static void xe_ring_finish(struct xe_ring *r)
-{
-	ttm_bo_kunmap(&r->kmap);
-	xe_bo_put(r->bo);
-}
-
-static uint32_t xe_ring_head(struct xe_ring *r)
-{
-	return xe_lrc_regs(r->lrc)[CTX_RING_HEAD];
-}
-
-static uint32_t xe_ring_space(struct xe_ring *r)
-{
-	return (r->tail - xe_ring_head(r)) & (r->size - 1);
-}
-
-static void xe_ring_write(struct xe_ring *r, const void *data, size_t size)
-{
-	bool is_iomem;
-	void *map;
-	size_t cpy_size;
-
-	XE_BUG_ON(size > r->size);
-	XE_WARN_ON(size > xe_ring_space(r));
-
-	map = ttm_kmap_obj_virtual(&r->kmap, &is_iomem);
-	WARN_ON_ONCE(is_iomem);
-
-	XE_BUG_ON(r->tail >= r->size);
-
-	cpy_size = min_t(size_t, size, r->size - r->tail);
-	memcpy(map + r->tail, data, cpy_size);
-	if (cpy_size < size)
-		memcpy(map, data + cpy_size, size - cpy_size);
-
-	r->tail = (r->tail + size) & (r->size - 1);
-	xe_lrc_regs(r->lrc)[CTX_RING_TAIL] = r->tail;
-}
-
 #if 0
+static void xe_execlist_port_wake_locked(struct xe_execlist_port *port)
+{
+}
+
 static void xe_execlist_make_active(struct xe_execlist *exl)
 {
 	struct xe_execlist_port *port = exl->port;
@@ -165,7 +95,6 @@ static const struct drm_sched_backend_ops drm_sched_ops = {
 
 struct xe_execlist *xe_execlist_create(struct xe_engine *e)
 {
-	struct xe_device *xe = e->hwe->xe;
 	struct drm_gpu_scheduler *sched;
 	struct xe_execlist *exl;
 	int err;
@@ -189,17 +118,10 @@ struct xe_execlist *xe_execlist_create(struct xe_engine *e)
 		goto err_sched;
 
 	exl->port = e->hwe->exl_port;
-
-	err = xe_ring_init(&exl->ring, xe, e->vm, &e->lrc, SZ_16K);
-	if (err)
-		goto err_entity;
-
 	exl->active_priority = DRM_SCHED_PRIORITY_UNSET;
 
 	return exl;
 
-err_entity:
-	drm_sched_entity_fini(&exl->entity);
 err_sched:
 	drm_sched_fini(&exl->sched);
 err_free:
@@ -209,7 +131,6 @@ err_free:
 
 void xe_execlist_destroy(struct xe_execlist *exl)
 {
-	xe_ring_finish(&exl->ring);
 	drm_sched_entity_fini(&exl->entity);
 	drm_sched_fini(&exl->sched);
 	kfree(exl);
