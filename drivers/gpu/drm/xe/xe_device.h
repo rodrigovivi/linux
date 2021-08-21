3fa6ab411ef34fdff6783ce81d96aa1f30457eff
Jason Ekstrand <jason@jlekstrand.net>
2021-08-20 23:30:34 -0500

    xe: Add a XE_IOCTL_ERR wrapper for easier userspace deubbing

--- drivers/gpu/drm/xe/xe_device.h
+++ drivers/gpu/drm/xe/xe_device.h
@@ -21,6 +21,12 @@
 #define XE_WARN_ON WARN_ON
 #define XE_BUG_ON BUG_ON
 
+#define XE_IOCTL_ERR(xe, cond) \
+	((cond) && (drm_dbg(&(xe)->drm, \
+			    "Ioctl argument check failed at %s:%d: %s", \
+			    __FILE__, __LINE__, #cond), 1))
+
+
 #define XE_BO_INVALID_OFFSET	LONG_MAX
 
 #define GRAPHICS_VER(xe) 12
