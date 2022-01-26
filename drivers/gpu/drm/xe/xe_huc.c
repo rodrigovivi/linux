c2ad95863342c383f5b7b2b24cf394704b994f7c
Matthew Brost <matthew.brost@intel.com>
2022-01-26 13:31:58 -0800

    drm/xe/uc: Move uC firmware fetch cleanup to GuC / HuC code
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_huc.c
+++ drivers/gpu/drm/xe/xe_huc.c
@@ -4,12 +4,21 @@
  */
 
 #include "xe_huc.h"
+#include "xe_uc_fw.h"
 
 int xe_huc_init(struct xe_huc *huc)
 {
+	int ret;
+
+	huc->fw.type = XE_UC_FW_TYPE_HUC;
+	ret = xe_uc_fw_init(&huc->fw);
+	if (ret)
+		return ret;
+
 	return 0;
 }
 
 void xe_huc_fini(struct xe_huc *huc)
 {
+	xe_uc_fw_fini(&huc->fw);
 }
