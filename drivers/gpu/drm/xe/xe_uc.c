d32adca9a50efadc56076ecaa0ffef265e86a15b
Matthew Brost <matthew.brost@intel.com>
2022-01-26 13:31:58 -0800

    drm/xe/uc: Move uC firmware fetch cleanup to GuC / HuC code
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_uc.c
+++ drivers/gpu/drm/xe/xe_uc.c
@@ -6,30 +6,11 @@
 #include "xe_guc.h"
 #include "xe_huc.h"
 #include "xe_uc.h"
-#include "xe_uc_fw.h"
 #include "xe_wopcm.h"
-
-static void uc_fetch_firmwares(struct xe_uc *uc)
-{
-	uc->guc.fw.type = XE_UC_FW_TYPE_GUC;
-	xe_uc_fw_init(&uc->guc.fw);
-
-	uc->huc.fw.type = XE_UC_FW_TYPE_HUC;
-	xe_uc_fw_init(&uc->huc.fw);
-}
-
-static void uc_cleanup_firmwares(struct xe_uc *uc)
-{
-	xe_uc_fw_fini(&uc->huc.fw);
-	xe_uc_fw_fini(&uc->guc.fw);
-}
 
 int xe_uc_init(struct xe_uc *uc)
 {
 	int ret;
-
-	uc_fetch_firmwares(uc);
-	xe_wopcm_init(&uc->wopcm);
 
 	ret = xe_guc_init(&uc->guc);
 	if (ret)
@@ -38,6 +19,8 @@ int xe_uc_init(struct xe_uc *uc)
 	ret = xe_huc_init(&uc->huc);
 	if (ret)
 		goto err_guc;
+
+	xe_wopcm_init(&uc->wopcm);
 
 	return 0;
 
@@ -50,5 +33,4 @@ void xe_uc_fini(struct xe_uc *uc)
 {
 	xe_huc_fini(&uc->huc);
 	xe_guc_fini(&uc->guc);
-	uc_cleanup_firmwares(uc);
 }
