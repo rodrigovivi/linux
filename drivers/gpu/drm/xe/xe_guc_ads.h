fe10977bbd50b04e9f08e8e9cda5b45e892eff40
Matthew Brost <matthew.brost@intel.com>
2022-01-26 15:01:35 -0800

    drm/xe/guc: Add GuC ads init / fini stubs
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

---
+++ drivers/gpu/drm/xe/xe_guc_ads.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright © 2022 Intel Corporation
+ */
+
+#ifndef _XE_GUC_ADS_H_
+#define _XE_GUC_ADS_H_
+
+#include "xe_guc_ads_types.h"
+
+int xe_guc_ads_init(struct xe_guc_ads *ads);
+void xe_guc_ads_fini(struct xe_guc_ads *ads);
+
+#endif	/* _XE_GUC_ADS_H_ */
