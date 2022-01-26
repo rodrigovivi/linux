50f24f6180c9eef99f2f9c899b0524a1d3e0e4ab
Matthew Brost <matthew.brost@intel.com>
2022-01-26 14:55:12 -0800

    drm/xe/guc: Add GuC logs init / fini stubs
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_guc_types.h
+++ drivers/gpu/drm/xe/xe_guc_types.h
@@ -8,6 +8,7 @@
 #define _XE_GUC_TYPES_H_
 
 #include "xe_uc_fw_types.h"
+#include "xe_guc_log_types.h"
 
 /**
  * struct xe_guc - Graphic micro controller
@@ -15,6 +16,8 @@
 struct xe_guc {
 	/** @fw: Generic uC firmware management */
 	struct xe_uc_fw fw;
+	/** @log: GuC log */
+	struct xe_guc_log log;
 };
 
 #endif	/* _XE_GUC_TYPES_H_ */
