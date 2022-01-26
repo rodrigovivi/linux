d0a976acfc9bb01e713071166bc847432e2246ff
Matthew Brost <matthew.brost@intel.com>
2022-01-26 13:27:25 -0800

    drm/xe/huc: Add HuC init / fini stubs
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

---
+++ drivers/gpu/drm/xe/xe_huc.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2022 Intel Corporation
+ */
+
+#include "xe_huc.h"
+
+int xe_huc_init(struct xe_huc *huc)
+{
+	return 0;
+}
+
+void xe_huc_fini(struct xe_huc *huc)
+{
+}
