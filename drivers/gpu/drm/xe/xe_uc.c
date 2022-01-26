e878fee7088f1951aceb39d1df0ce176b58cd97a
Matthew Brost <matthew.brost@intel.com>
2022-01-26 13:27:25 -0800

    drm/xe/huc: Add HuC init / fini stubs
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_uc.c
+++ drivers/gpu/drm/xe/xe_uc.c
@@ -4,6 +4,7 @@
  */
 
 #include "xe_guc.h"
+#include "xe_huc.h"
 #include "xe_uc.h"
 #include "xe_uc_fw.h"
 #include "xe_wopcm.h"
@@ -34,11 +35,20 @@ int xe_uc_init(struct xe_uc *uc)
 	if (ret)
 		return ret;
 
+	ret = xe_huc_init(&uc->huc);
+	if (ret)
+		goto err_guc;
+
 	return 0;
+
+err_guc:
+	xe_guc_fini(&uc->guc);
+	return ret;
 }
 
 void xe_uc_fini(struct xe_uc *uc)
 {
+	xe_huc_fini(&uc->huc);
 	xe_guc_fini(&uc->guc);
 	uc_cleanup_firmwares(uc);
 }
