adbc51fb8055987cc31365aaa3b01eb798012e07
Matthew Brost <matthew.brost@intel.com>
2022-01-26 13:26:13 -0800

    drm/xe/guc: Add GuC init / fini stubs
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

---
+++ drivers/gpu/drm/xe/xe_guc.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2022 Intel Corporation
+ */
+
+#include "xe_guc.h"
+
+int xe_guc_init(struct xe_guc *guc)
+{
+	return 0;
+}
+
+void xe_guc_fini(struct xe_guc *guc)
+{
+}
--- drivers/gpu/drm/xe/xe_uc.c
+++ drivers/gpu/drm/xe/xe_uc.c
@@ -3,6 +3,7 @@
  * Copyright © 2022 Intel Corporation
  */
 
+#include "xe_guc.h"
 #include "xe_uc.h"
 #include "xe_uc_fw.h"
 #include "xe_wopcm.h"
@@ -24,13 +25,20 @@ static void uc_cleanup_firmwares(struct xe_uc *uc)
 
 int xe_uc_init(struct xe_uc *uc)
 {
+	int ret;
+
 	uc_fetch_firmwares(uc);
 	xe_wopcm_init(&uc->wopcm);
+
+	ret = xe_guc_init(&uc->guc);
+	if (ret)
+		return ret;
 
 	return 0;
 }
 
 void xe_uc_fini(struct xe_uc *uc)
 {
+	xe_guc_fini(&uc->guc);
 	uc_cleanup_firmwares(uc);
 }
