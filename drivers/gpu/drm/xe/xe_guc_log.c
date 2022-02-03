54ed688ce4b64a27c8948a3aa63910020ca4c424
Matthew Brost <matthew.brost@intel.com>
2022-02-02 17:25:33 -0800

    drm/xe: Dump GuC log to dmesg on load / auth failure
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_guc_log.c
+++ drivers/gpu/drm/xe/xe_guc_log.c
@@ -38,6 +38,34 @@ log_to_xe(struct xe_guc_log *log)
 	return container_of(log, struct xe_device, uc.guc.log);
 }
 
+void xe_guc_log_dump(struct xe_guc_log *log, struct drm_printer *p)
+{
+	struct dma_buf_map map;
+	size_t size;
+	int i, j;
+
+	XE_BUG_ON(!log->bo);
+
+	map = log->bo->vmap;
+	size = log->bo->size;
+
+#define DW_PER_PRINT		4
+	XE_BUG_ON(size % (DW_PER_PRINT * sizeof(u32)));
+	for (i = 0; i < size / sizeof(u32); i += DW_PER_PRINT) {
+		u32 read[DW_PER_PRINT];
+
+		for (j = 0; j < DW_PER_PRINT; ++j) {
+			read[j] = dbm_read32(map);
+			dma_buf_map_incr(&map, sizeof(u32));
+		}
+
+		drm_printf(p, "0x%08x 0x%08x 0x%08x 0x%08x\n",
+			   *(read + 0), *(read + 1),
+			   *(read + 2), *(read + 3));
+	}
+#undef DW_PER_PRINT
+}
+
 int xe_guc_log_init(struct xe_guc_log *log)
 {
 	struct xe_device *xe = log_to_xe(log);
