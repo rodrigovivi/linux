5288d86a58c7a66b2d224e067a5fa5f92b1913d6
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2022-02-03 12:11:58 +0100

    drm/xe: Add kernel batchbuffer submission
    Create a xe_sched_job when submitting, this can be used to add dependencies,
    and fire off the batch buffer.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

---
+++ drivers/gpu/drm/xe/xe_bb.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright © 2022 Intel Corporation
+ */
+
+#ifndef _XE_BB_H_
+#define _XE_BB_H_
+
+#include "xe_bb_types.h"
+
+struct dma_fence;
+
+struct xe_gt;
+struct xe_engine;
+struct xe_sched_job;
+
+struct xe_bb *xe_bb_new(struct xe_gt *gt, u32 size);
+struct xe_sched_job *xe_bb_create_job(struct xe_engine *kernel_eng, struct xe_bb *bb);
+void xe_bb_free(struct xe_bb *bb, struct dma_fence *fence);
+
+#endif
