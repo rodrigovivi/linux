9200c2fd85f45212a23f2b6b777619da609126fa
Matthew Brost <matthew.brost@intel.com>
2022-02-07 11:54:19 -0800

    drm/xe: Add for_each_hw_engine iterator macro
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_gt.h
+++ drivers/gpu/drm/xe/xe_gt.h
@@ -7,8 +7,15 @@
 #ifndef _XE_GT_H_
 #define _XE_GT_H_
 
+#include <drm/drm_util.h>
+
 #include "xe_device_types.h"
 #include "xe_hw_engine.h"
+
+#define for_each_hw_engine(hwe__, gt__, id__) \
+	for ((id__) = 0; (id__) < ARRAY_SIZE((gt__)->hw_engines); (id__)++) \
+	     for_each_if (((hwe__) = (gt__)->hw_engines + (id__)) && \
+			  xe_hw_engine_is_valid((hwe__)))
 
 int xe_gt_alloc(struct xe_gt *gt);
 int xe_gt_init(struct xe_gt *gt);
