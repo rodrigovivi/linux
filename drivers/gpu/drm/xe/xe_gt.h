343f871494090a296250e8d1e59ce992c206b89c
Matthew Brost <matthew.brost@intel.com>
2022-03-24 10:55:23 -0700

    drm/xe: Pull in some missing fixes from last MR in 1 patch
    Fix kernel engine resets
    Fix parallel submit memory leak
    Use ordered work queue for GT resets + TDR
    Write memory barrier usage
    Delete some unused code from the DRM scheduler
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_gt.h
+++ drivers/gpu/drm/xe/xe_gt.h
@@ -19,7 +19,6 @@
 
 int xe_gt_alloc(struct xe_gt *gt);
 int xe_gt_init(struct xe_gt *gt);
-int xe_gt_reset(struct xe_gt *gt);
 void xe_gt_reset_async(struct xe_gt *gt);
 
 struct xe_hw_engine *xe_gt_hw_engine(struct xe_gt *gt,
