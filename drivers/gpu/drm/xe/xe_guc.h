d392dba55098e780fd702b85ff842f728948d5fe
Matthew Brost <matthew.brost@intel.com>
2022-03-25 11:05:01 -0700

    drm/xe: Add GuC defs for compute engine class
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_guc.h
+++ drivers/gpu/drm/xe/xe_guc.h
@@ -41,8 +41,9 @@ static inline u16 xe_engine_class_to_guc_class(enum xe_engine_class class)
 		return GUC_VIDEOENHANCE_CLASS;
 	case XE_ENGINE_CLASS_COPY:
 		return GUC_BLITTER_CLASS;
-	case XE_ENGINE_CLASS_OTHER:
 	case XE_ENGINE_CLASS_COMPUTE:
+		return GUC_COMPUTE_CLASS;
+	case XE_ENGINE_CLASS_OTHER:
 	default:
 		XE_WARN_ON(class);
 		return -1;
