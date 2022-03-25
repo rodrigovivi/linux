7916a3526235416eb341eb6dfa5549e08188bbf7
Matthew Brost <matthew.brost@intel.com>
2022-03-24 17:50:02 -0700

    drm/xe: Implement user fence sync type
    Also don't allow any other sync types on compute engines.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_sync.h
+++ drivers/gpu/drm/xe/xe_sync.h
@@ -9,10 +9,12 @@ struct xe_sched_job;
 
 int xe_sync_entry_parse(struct xe_device *xe, struct xe_file *xef,
 			struct xe_sync_entry *sync,
-			struct drm_xe_sync __user *sync_user);
+			struct drm_xe_sync __user *sync_user,
+			bool exec, bool compute);
 int xe_sync_entry_add_deps(struct xe_sync_entry *sync,
 			   struct xe_sched_job *job);
 void xe_sync_entry_signal(struct xe_sync_entry *sync,
+			  struct xe_sched_job *job,
 			  struct dma_fence *fence);
 void xe_sync_entry_cleanup(struct xe_sync_entry *sync);
 
