6a9760ee164a412dd52e7c7f727cf89166d661db
Philippe Lecluse <philippe.lecluse@intel.com>
2022-04-11 07:14:47 +0000

    drm/xe: enable non-x86 platforms
    Build successfully on arm64
    
    Signed-off-by: Philippe Lecluse <philippe.lecluse@intel.com>

--- drivers/gpu/drm/xe/xe_bo.h
+++ drivers/gpu/drm/xe/xe_bo.h
@@ -20,6 +20,43 @@
 	(IS_DGFX(xe) ? XE_BO_CREATE_VRAM_BIT : XE_BO_CREATE_SYSTEM_BIT)
 #define XE_BO_CREATE_GGTT_BIT BIT(4)
 
+#if  !defined(CONFIG_X86)
+	#define _PAGE_BIT_PRESENT       0       /* is present */
+	#define _PAGE_BIT_RW            1       /* writeable */
+	#define _PAGE_BIT_PWT           3       /* page write through */
+	#define _PAGE_BIT_PCD           4       /* page cache disabled */
+	#define _PAGE_BIT_PAT           7       /* on 4KB pages */
+
+	#define _PAGE_PRESENT   (_AT(pteval_t, 1) << _PAGE_BIT_PRESENT)
+	#define _PAGE_RW        (_AT(pteval_t, 1) << _PAGE_BIT_RW)
+	#define _PAGE_PWT       (_AT(pteval_t, 1) << _PAGE_BIT_PWT)
+	#define _PAGE_PCD       (_AT(pteval_t, 1) << _PAGE_BIT_PCD)
+	#define _PAGE_PAT       (_AT(pteval_t, 1) << _PAGE_BIT_PAT)
+#endif
+
+#define PPAT_UNCACHED                   (_PAGE_PWT | _PAGE_PCD)
+#define PPAT_CACHED_PDE                 0 /* WB LLC */
+#define PPAT_CACHED                     _PAGE_PAT /* WB LLCeLLC */
+#define PPAT_DISPLAY_ELLC               _PAGE_PCD /* WT eLLC */
+
+#define GEN8_PTE_SHIFT			12
+#define GEN8_PAGE_SIZE			(1 << GEN8_PTE_SHIFT)
+#define GEN8_PTE_MASK			(GEN8_PAGE_SIZE - 1)
+#define GEN8_PDE_SHIFT			(GEN8_PTE_SHIFT - 3)
+#define GEN8_PDES			(1 << GEN8_PDE_SHIFT)
+#define GEN8_PDE_MASK			(GEN8_PDES - 1)
+
+#define GEN8_PDE_PS_2M			BIT(7)
+#define GEN8_PDE_IPS_64K		BIT(11)
+
+#define GEN12_GGTT_PTE_LM		BIT_ULL(1)
+#define GEN12_PPGTT_PTE_LM		BIT_ULL(11)
+
+#define GEN8_PAGE_PRESENT		BIT_ULL(0)
+#define GEN8_PAGE_RW			BIT_ULL(1)
+
+#define PTE_READ_ONLY			BIT(0)
+
 struct xe_bo *xe_bo_create_locked(struct xe_device *xe,
 				  struct xe_vm *vm, size_t size,
 				  enum ttm_bo_type type, uint32_t flags);
