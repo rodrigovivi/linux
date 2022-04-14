61fd471f560f8ca81f0b6c3922cfe1a9dffd2b7f
Matthew Brost <matthew.brost@intel.com>
2022-04-14 15:27:08 -0700

    drm/xe: Update uAPI for VM bind op error reporting
    Also add compute mode VM flag, s/compute/compute_mode for engine flag,
    and async bind op flag.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_sync.h
+++ drivers/gpu/drm/xe/xe_sync.h
@@ -10,7 +10,7 @@ struct xe_sched_job;
 int xe_sync_entry_parse(struct xe_device *xe, struct xe_file *xef,
 			struct xe_sync_entry *sync,
 			struct drm_xe_sync __user *sync_user,
-			bool exec, bool compute);
+			bool exec, bool compute_mode);
 int xe_sync_entry_add_deps(struct xe_sync_entry *sync,
 			   struct xe_sched_job *job);
 bool xe_sync_entry_signal(struct xe_sync_entry *sync,
