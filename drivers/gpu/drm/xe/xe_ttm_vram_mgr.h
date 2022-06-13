08dfb3af8cb8a92eb52a69ca7c316efa1254d5da
Matthew Brost <matthew.brost@intel.com>
2022-06-13 16:27:19 -0700

    drm/xe: Update xe_dma_buf_map / xe_dma_buf_unmap for VRAM objects
    Again shameless copy from AMD.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_ttm_vram_mgr.h
+++ drivers/gpu/drm/xe/xe_ttm_vram_mgr.h
@@ -12,5 +12,13 @@
 struct xe_gt;
 
 int xe_ttm_vram_mgr_init(struct xe_gt *gt, struct xe_ttm_vram_mgr *mgr);
+int xe_ttm_vram_mgr_alloc_sgt(struct xe_device *xe,
+			      struct ttm_resource *res,
+			      u64 offset, u64 length,
+			      struct device *dev,
+			      enum dma_data_direction dir,
+			      struct sg_table **sgt);
+void xe_ttm_vram_mgr_free_sgt(struct device *dev, enum dma_data_direction dir,
+			      struct sg_table *sgt);
 
 #endif	/* _XE_TTM_VRAM_MGR_H_ */
