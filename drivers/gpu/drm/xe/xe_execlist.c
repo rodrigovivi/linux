0e8078f6672358ab8bf6d24d2f4987b78e9b236e
Philippe Lecluse <philippe.lecluse@intel.com>
2022-06-14 09:24:20 +0000

    drm/xe: enable compute mode on PVC
    Turn on RCU Mode in exec list
    Fill ads regsave RCU Mode in guc mode
    
    Signed-off-by: Philippe Lecluse <philippe.lecluse@intel.com>

--- drivers/gpu/drm/xe/xe_execlist.c
+++ drivers/gpu/drm/xe/xe_execlist.c
@@ -60,6 +60,10 @@ static void __start_lrc(struct xe_hw_engine *hwe, struct xe_lrc *lrc,
 		lrc_desc |= FIELD_PREP(GEN11_SW_CTX_ID, ctx_id);
 	}
 
+	if (hwe->class == XE_ENGINE_CLASS_COMPUTE)
+		xe_mmio_write32(hwe->gt, GEN12_RCU_MODE.reg,
+				_MASKED_BIT_ENABLE(GEN12_RCU_MODE_CCS_ENABLE));
+
 	xe_lrc_write_ctx_reg(lrc, CTX_RING_TAIL, lrc->ring.tail);
 	lrc->ring.old_tail = lrc->ring.tail;
 
