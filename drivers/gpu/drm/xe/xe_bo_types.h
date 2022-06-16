5aface23e9df5eba385cd9d0655cd9e4f0c4f44a
Matthew Brost <matthew.brost@intel.com>
2022-06-16 15:44:11 -0700

    drm/xe: Update xe_bo_types.h kernel doc
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_bo_types.h
+++ drivers/gpu/drm/xe/xe_bo_types.h
@@ -20,24 +20,26 @@ struct xe_vm;
 
 #define XE_BO_MAX_PLACEMENTS	3
 
+/** @xe_bo: XE buffer object */
 struct xe_bo {
+	/** @ttm: TTM base buffer object */
 	struct ttm_buffer_object ttm;
-
+	/** @size: Size of this buffer object */
 	size_t size;
-
+	/** @flags: flags for this buffer object */
 	uint32_t flags;
-
+	/** @vm: VM this BO is attached to, for extobj this will be NULL */
 	struct xe_vm *vm;
-
+	/** @vmas: List of VMAs for this BO */
 	struct list_head vmas;
-
+	/** @placements: valid placements for this BO */
 	struct ttm_place placements[XE_BO_MAX_PLACEMENTS];
+	/** @placement: current placement for this BO */
 	struct ttm_placement placement;
-
+	/** @ggtt_node: GGTT node if this BO is mapped in the GGTT */
 	struct drm_mm_node ggtt_node;
-
+	/** @vmap: iosys map of this buffer */
 	struct iosys_map vmap;
-
 	/** @extobj_tv: used during exec to lock all external BOs */
 	struct ttm_validate_buffer extobj_tv;
 };
