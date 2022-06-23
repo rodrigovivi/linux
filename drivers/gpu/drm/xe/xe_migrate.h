24c7a89dfe47968d22fc1dadff4fd1cd3695b372
Matthew Brost <matthew.brost@intel.com>
2022-06-23 00:37:13 -0700

    drm/xe: Delete bogus wait on preempt fences in bind
    Moves / userptr invalidations trigger preempt fences.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_migrate.h
+++ drivers/gpu/drm/xe/xe_migrate.h
@@ -43,7 +43,6 @@ xe_migrate_update_pgtables(struct xe_migrate *m,
 			   struct xe_vm_pgtable_update *updates,
 			   u32 num_updates,
 			   struct xe_sync_entry *syncs, u32 num_syncs,
-			   xe_migrate_populatefn_t populatefn, void *arg,
-			   bool wait_preempt);
+			   xe_migrate_populatefn_t populatefn, void *arg);
 
 #endif
