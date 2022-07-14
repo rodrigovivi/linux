4bd84cf322856a9b329aa4195d013a2ded200da6
Lucas De Marchi <lucas.demarchi@intel.com>
2022-07-14 09:45:11 -0700

    drm/xe: Add initial set of workarounds
    xe_wa.c is dedicated for the workarounds, just re-using the xe-rtp
    (register table poke) and xe-reg-sr (reg save-restore). Table contains
    the initial workarounds for DG2 with the rules where they apply.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

---
+++ drivers/gpu/drm/xe/xe_wa.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2022 Intel Corporation
+ */
+
+#ifndef _XE_WA_
+#define _XE_WA_
+
+struct xe_gt;
+struct xe_hw_engine;
+
+void xe_wa_process_gt(struct xe_gt *gt);
+void xe_wa_process_engine(struct xe_hw_engine *hwe);
+void xe_wa_process_ctx(struct xe_hw_engine *hwe);
+
+#endif
