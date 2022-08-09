8552de305489a791583c569b62e589b7db92853f
Rodrigo Vivi <rodrigo.vivi@intel.com>
2022-08-09 13:59:19 -0400

    drm/xe: Add GT frequency sanity and management with GuC PC.
    First of all this patch brings some sanity to the frequencies that
    GuC is selecting. Right now GuC selects some really high RPMax
    frequency as max default. This doesn't make any sense without any
    overclocking. Let's bring the maximum to the hardware RP0.
    
    Then, let's also introduce the sysfs interfaces for controlling these
    frequency limits.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_guc_pc_types.h
+++ drivers/gpu/drm/xe/xe_guc_pc_types.h
@@ -6,12 +6,20 @@
 #ifndef _XE_GUC_PC_TYPES_H_
 #define _XE_GUC_PC_TYPES_H_
 
+#include <linux/types.h>
+
 /**
  * struct xe_guc_pc - GuC Power Conservation (PC)
  */
 struct xe_guc_pc {
 	/** @bo: GGTT buffer object that is shared with GuC PC */
 	struct xe_bo *bo;
+	/** @rp0_freq: HW RP0 frequency - The Maximum one */
+	u32 rp0_freq;
+	/** @rpe_freq: HW RPe frequency - The Efficient one */
+	u32 rpe_freq;
+	/** @rpn_freq: HW RPN frequency - The Minimum one */
+	u32 rpn_freq;
 };
 
 #endif	/* _XE_GUC_PC_TYPES_H_ */
