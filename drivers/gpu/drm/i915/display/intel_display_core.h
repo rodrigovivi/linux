95183816a8a1a8722c6d61d880ab9ecdb3dcbaa7
Jani Nikula <jani.nikula@intel.com>
2022-08-11 18:07:23 +0300

    drm/i915: move and split audio under display.audio and display.funcs
    Move display related members under drm_i915_private display sub-struct.
    
    Split audio funcs to display.funcs to follow the same pattern as all the
    other display functions.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Arun R Murthy <arun.r.murthy@intel.com>
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/d7de23e80cb3a3fb488b76a092ad5f6d94d97416.1660230121.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_display_core.h
+++ drivers/gpu/drm/i915/display/intel_display_core.h
@@ -10,11 +10,14 @@
 #include <linux/types.h>
 #include <linux/wait.h>
 
+#include "intel_display.h"
 #include "intel_dmc.h"
 #include "intel_gmbus.h"
 
 struct drm_i915_private;
+struct i915_audio_component;
 struct intel_atomic_state;
+struct intel_audio_funcs;
 struct intel_cdclk_funcs;
 struct intel_clock_gating_funcs;
 struct intel_color_funcs;
@@ -56,6 +59,25 @@ struct intel_wm_funcs {
 	int (*compute_global_watermarks)(struct intel_atomic_state *state);
 };
 
+struct intel_audio {
+	/* hda/i915 audio component */
+	struct i915_audio_component *component;
+	bool component_registered;
+	/* mutex for audio/video sync */
+	struct mutex mutex;
+	int power_refcount;
+	u32 freq_cntrl;
+
+	/* Used to save the pipe-to-encoder mapping for audio */
+	struct intel_encoder *encoder_map[I915_MAX_PIPES];
+
+	/* necessary resource sharing with HDMI LPE audio driver. */
+	struct {
+		struct platform_device *platdev;
+		int irq;
+	} lpe;
+};
+
 struct intel_display {
 	/* Display functions */
 	struct {
@@ -82,6 +104,9 @@ struct intel_display {
 
 		/* Display internal color functions */
 		const struct intel_color_funcs *color;
+
+		/* Display internal audio functions */
+		const struct intel_audio_funcs *audio;
 	} funcs;
 
 	/* Grouping using anonymous structs. Keep sorted. */
@@ -111,6 +136,7 @@ struct intel_display {
 	} pps;
 
 	/* Grouping using named structs. Keep sorted. */
+	struct intel_audio audio;
 	struct intel_dmc dmc;
 };
 
