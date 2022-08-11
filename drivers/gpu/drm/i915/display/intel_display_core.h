a56d131689abe32a68d1f92adbcd02f38af817fd
Jani Nikula <jani.nikula@intel.com>
2022-08-11 18:07:15 +0300

    drm/i915: move hotplug_funcs to display.funcs
    Move display related members under drm_i915_private display sub-struct.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Arun R Murthy <arun.r.murthy@intel.com>
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/664ed8395a6028e654cbc2251605e967a9d46289.1660230121.git.jani.nikula@intel.com

--- drivers/gpu/drm/i915/display/intel_display_core.h
+++ drivers/gpu/drm/i915/display/intel_display_core.h
@@ -13,6 +13,7 @@ struct intel_cdclk_funcs;
 struct intel_crtc;
 struct intel_crtc_state;
 struct intel_dpll_funcs;
+struct intel_hotplug_funcs;
 struct intel_initial_plane_config;
 
 struct intel_display_funcs {
@@ -40,6 +41,9 @@ struct intel_display {
 
 		/* Display pll funcs */
 		const struct intel_dpll_funcs *dpll;
+
+		/* irq display functions */
+		const struct intel_hotplug_funcs *hotplug;
 	} funcs;
 };
 
