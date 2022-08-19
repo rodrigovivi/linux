5e100933ce3f58b9280410526dfb035e61d2df79
Matthew Brost <matthew.brost@intel.com>
2022-08-19 16:38:44 -0700

    drm/xe: Add evict debugfs entry for quick selftest
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_debugfs.c
+++ drivers/gpu/drm/xe/xe_debugfs.c
@@ -10,6 +10,12 @@
 #include "xe_debugfs.h"
 #include "xe_gt_debugfs.h"
 
+#ifdef CONFIG_DRM_XE_DEBUG
+#include "xe_bo_evict.h"
+#include "xe_migrate.h"
+#include "xe_vm.h"
+#endif
+
 static struct xe_device *node_to_xe(struct drm_info_node *node)
 {
 	return to_xe_device(node->minor->dev);
@@ -38,8 +44,113 @@ static int info(struct seq_file *m, void *data)
 	return 0;
 }
 
+#ifdef CONFIG_DRM_XE_DEBUG
+static int evict_selftest(struct seq_file *m, void *data)
+{
+	struct xe_device *xe = node_to_xe(m->private);
+	struct drm_printer p = drm_seq_file_printer(m);
+	struct xe_bo *bo, *external;
+	unsigned bo_flags = XE_BO_CREATE_USER_BIT |
+		XE_BO_CREATE_VRAM0_BIT | XE_BO_INTERNAL_TEST;
+	struct xe_vm *vm = xe_migrate_get_vm(xe->gt[0].migrate);
+	int err, i;
+
+	if (!IS_DGFX(xe))
+		return 0;
+
+	for (i = 0; i < 2; ++i) {
+		bo = xe_bo_create(xe, NULL, vm, 0x10000, ttm_bo_type_device,
+				  bo_flags);
+		if (IS_ERR(bo)) {
+			drm_printf(&p, "bo create err=%ld\n", PTR_ERR(bo));
+			break;
+		}
+
+		external = xe_bo_create(xe, NULL, NULL, 0x10000,
+					ttm_bo_type_device, bo_flags);
+		if (IS_ERR(external)) {
+			drm_printf(&p, "external bo create err=%ld\n",
+				   PTR_ERR(external));
+			goto cleanup_bo;
+		}
+
+		err = xe_bo_pin_external(external);
+		if (err) {
+			drm_printf(&p, "external bo pin err=%d\n", err);
+			goto cleanup_external;
+		}
+
+		err = xe_bo_evict_all(xe);
+		if (err) {
+			drm_printf(&p, "evict err=%d\n", err);
+			goto cleanup_all;
+		}
+
+		err = xe_bo_restore_kernel(xe);
+		if (err) {
+			drm_printf(&p, "restore kernel err=%d\n", err);
+			goto cleanup_all;
+		}
+
+		err = xe_bo_restore_user(xe);
+		if (err) {
+			drm_printf(&p, "restore user err=%d\n", err);
+			goto cleanup_all;
+		}
+
+		if (!xe_bo_is_vram(external)) {
+			drm_printf(&p, "external bo not is vram\n");
+			err = -EPROTO;
+			goto cleanup_all;
+		}
+
+		if (xe_bo_is_vram(bo)) {
+			drm_printf(&p, "bo is vram\n");
+			err = -EPROTO;
+			goto cleanup_all;
+		}
+
+		if (i) {
+			err = xe_bo_validate(bo, bo->vm);
+			if (err) {
+				drm_printf(&p, "bo valid err=%d\n", err);
+				goto cleanup_all;
+			}
+			err = xe_bo_validate(external, NULL);
+			if (err) {
+				drm_printf(&p, "external bo valid err=%d\n",
+					   err);
+				goto cleanup_all;
+			}
+		}
+
+		xe_bo_unpin_external(external);
+		xe_bo_put(external);
+		xe_bo_put(bo);
+		continue;
+
+cleanup_all:
+		xe_bo_unpin_external(external);
+cleanup_external:
+		xe_bo_put(external);
+cleanup_bo:
+		xe_bo_put(bo);
+		break;
+	}
+
+	xe_vm_put(vm);
+	if (!err)
+		drm_printf(&p, "evict selftest passed\n");
+
+	return 0;
+}
+#endif
+
 static const struct drm_info_list debugfs_list[] = {
 	{"info", info, 0},
+#ifdef CONFIG_DRM_XE_DEBUG
+	{"evict_selftest", evict_selftest, 0},
+#endif
 };
 
 void xe_debugfs_register(struct xe_device *xe)
