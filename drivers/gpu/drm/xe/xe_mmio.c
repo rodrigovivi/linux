73369bd0e00c317f8bc5b157cd1bf45208d93586
Rodrigo Vivi <rodrigo.vivi@intel.com>
2022-09-24 00:48:05 -0400

    drm/xe: Stop inlining the mmio functions.
    We will soon need to get forcewake domains from within them.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_mmio.c
+++ drivers/gpu/drm/xe/xe_mmio.c
@@ -278,3 +278,65 @@ int xe_mmio_ioctl(struct drm_device *dev, void *data,
 
 	return 0;
 }
+
+void xe_mmio_write32(struct xe_gt *gt, u32 reg, u32 val)
+{
+	if (reg < gt->mmio.adj_limit)
+		reg += gt->mmio.adj_offset;
+
+	writel(val, gt->mmio.regs + reg);
+}
+
+u32 xe_mmio_read32(struct xe_gt *gt, u32 reg)
+{
+	if (reg < gt->mmio.adj_limit)
+		reg += gt->mmio.adj_offset;
+
+	return readl(gt->mmio.regs + reg);
+}
+
+void xe_mmio_rmw32(struct xe_gt *gt, u32 reg, u32 mask, u32 val)
+{
+	u32 reg_val;
+
+	reg_val = xe_mmio_read32(gt, reg);
+	reg_val &= mask;
+	reg_val |= val;
+	xe_mmio_write32(gt, reg, reg_val);
+}
+
+void xe_mmio_write64(struct xe_gt *gt, u32 reg, u64 val)
+{
+	if (reg < gt->mmio.adj_limit)
+		reg += gt->mmio.adj_offset;
+
+	writeq(val, gt->mmio.regs + reg);
+}
+
+u64 xe_mmio_read64(struct xe_gt *gt, u32 reg)
+{
+	if (reg < gt->mmio.adj_limit)
+		reg += gt->mmio.adj_offset;
+
+	return readq(gt->mmio.regs + reg);
+}
+
+int xe_mmio_write32_and_verify(struct xe_gt *gt,
+			       u32 reg, u32 val,
+			       u32 mask, u32 eval)
+{
+	u32 reg_val;
+
+	xe_mmio_write32(gt, reg, val);
+	reg_val = xe_mmio_read32(gt, reg);
+
+	return (reg_val & mask) != eval ? -EINVAL : 0;
+}
+
+int xe_mmio_wait32(struct xe_gt *gt,
+		   u32 reg, u32 val,
+		   u32 mask, u32 timeout_ms)
+{
+	return wait_for((xe_mmio_read32(gt, reg) & mask) == val,
+			timeout_ms);
+}
--- drivers/gpu/drm/xe/xe_mmio.h
+++ drivers/gpu/drm/xe/xe_mmio.h
@@ -22,72 +22,17 @@ struct drm_file;
 struct xe_device;
 
 int xe_mmio_init(struct xe_device *xe);
-
-static inline void xe_mmio_write32(struct xe_gt *gt,
-				   u32 reg, u32 val)
-{
-	if (reg < gt->mmio.adj_limit)
-		reg += gt->mmio.adj_offset;
-
-	writel(val, gt->mmio.regs + reg);
-}
-
-static inline u32 xe_mmio_read32(struct xe_gt *gt, u32 reg)
-{
-	if (reg < gt->mmio.adj_limit)
-		reg += gt->mmio.adj_offset;
-
-	return readl(gt->mmio.regs + reg);
-}
-
-static inline void xe_mmio_rmw32(struct xe_gt *gt, u32 reg, u32 mask,
-				 u32 val)
-{
-	u32 reg_val;
-
-	reg_val = xe_mmio_read32(gt, reg);
-	reg_val &= mask;
-	reg_val |= val;
-	xe_mmio_write32(gt, reg, reg_val);
-}
-
-static inline void xe_mmio_write64(struct xe_gt *gt,
-				   u32 reg, u64 val)
-{
-	if (reg < gt->mmio.adj_limit)
-		reg += gt->mmio.adj_offset;
-
-	writeq(val, gt->mmio.regs + reg);
-}
-
-static inline u64 xe_mmio_read64(struct xe_gt *gt, u32 reg)
-{
-	if (reg < gt->mmio.adj_limit)
-		reg += gt->mmio.adj_offset;
-
-	return readq(gt->mmio.regs + reg);
-}
-
-static inline int xe_mmio_write32_and_verify(struct xe_gt *gt,
-					     u32 reg, u32 val,
-					     u32 mask, u32 eval)
-{
-	u32 reg_val;
-
-	xe_mmio_write32(gt, reg, val);
-	reg_val = xe_mmio_read32(gt, reg);
-
-	return (reg_val & mask) != eval ? -EINVAL : 0;
-}
-
-static inline int xe_mmio_wait32(struct xe_gt *gt,
-				 u32 reg, u32 val,
-				 u32 mask, u32 timeout_ms)
-{
-	return wait_for((xe_mmio_read32(gt, reg) & mask) == val,
-			timeout_ms);
-}
-
+void xe_mmio_write32(struct xe_gt *gt, u32 reg, u32 val);
+u32 xe_mmio_read32(struct xe_gt *gt, u32 reg);
+void xe_mmio_rmw32(struct xe_gt *gt, u32 reg, u32 mask, u32 val);
+void xe_mmio_write64(struct xe_gt *gt, u32 reg, u64 val);
+u64 xe_mmio_read64(struct xe_gt *gt, u32 reg);
+int xe_mmio_write32_and_verify(struct xe_gt *gt,
+			       u32 reg, u32 val,
+			       u32 mask, u32 eval);
+int xe_mmio_wait32(struct xe_gt *gt,
+		   u32 reg, u32 val,
+		   u32 mask, u32 timeout_ms);
 int xe_mmio_ioctl(struct drm_device *dev, void *data,
 		  struct drm_file *file);
 
