b83deec13dae0697721d9875e1e093704186acc5
Lucas De Marchi <lucas.demarchi@intel.com>
2022-11-04 14:16:10 -0700

    drm/xe/pm: Fix missing return description
    drivers/gpu/drm/xe/xe_pm.c:38: info: Scanning doc for function xe_pm_suspend
    drivers/gpu/drm/xe/xe_pm.c:42: warning: No description found for return value of 'xe_pm_suspend'
    .. c:function:: int xe_pm_suspend (struct xe_device *xe)
    
    drivers/gpu/drm/xe/xe_pm.c:67: info: Scanning doc for function xe_pm_resume
    drivers/gpu/drm/xe/xe_pm.c:71: warning: No description found for return value of 'xe_pm_resume'
    .. c:function:: int xe_pm_resume (struct xe_device *xe)
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/xe_pm.c
+++ drivers/gpu/drm/xe/xe_pm.c
@@ -37,6 +37,8 @@
 /**
  * xe_pm_suspend - Helper for System suspend, i.e. S0->S3 / S0->S2idle
  * @xe: xe device instance
+ *
+ * Return: 0 on success
  */
 int xe_pm_suspend(struct xe_device *xe)
 {
@@ -66,6 +68,8 @@ int xe_pm_suspend(struct xe_device *xe)
 /**
  * xe_pm_suspend - Helper for System resume S3->S0 / S2idle->S0
  * @xe: xe device instance
+ *
+ * Return: 0 on success
  */
 int xe_pm_resume(struct xe_device *xe)
 {
