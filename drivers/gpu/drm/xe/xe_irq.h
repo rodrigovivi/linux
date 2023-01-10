d4853f1d8c887c38b88f9a7bb564b8c9a11b1e50
Matthew Brost <matthew.brost@intel.com>
2023-01-10 14:16:06 -0800

    drm/xe: Don't use engine_mask until after hwconfig parse
    The IRQ and MCR init used engine_mask before hwconfig parse (currently
    faked), move these steps after the hwconfig parse.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_irq.h
+++ drivers/gpu/drm/xe/xe_irq.h
@@ -7,8 +7,10 @@
 #define _XE_IRQ_H_
 
 struct xe_device;
+struct xe_gt;
 
 int xe_irq_install(struct xe_device *xe);
+void xe_gt_irq_postinstall(struct xe_gt *gt);
 void xe_irq_shutdown(struct xe_device *xe);
 void xe_irq_suspend(struct xe_device *xe);
 void xe_irq_resume(struct xe_device *xe);
