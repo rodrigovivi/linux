64b851ab618a00cd7806fe57e6195738d44ab12e
Lucas De Marchi <lucas.demarchi@intel.com>
2023-01-23 16:35:17 -0800

    drm/xe/reg_sr: Stop using i915_reg_t
    i915_reg_t is not representative of all the registers anymore.  i915
    also has i915_mcr_reg_t for some of them. For the rtp and reg_sr infra
    in xe, defining a separate type for them is not beneficial.
    
    For reg_sr, the use of i915_reg_t is mainly to create an index for the
    xarray. Any additional data can be stored in the entry.  The index can
    be just a plain u32.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_reg_sr.h
+++ drivers/gpu/drm/xe/xe_reg_sr.h
@@ -6,7 +6,6 @@
 #ifndef _XE_REG_SR_
 #define _XE_REG_SR_
 
-#include "i915_reg_defs.h"
 #include "xe_reg_sr_types.h"
 
 /*
@@ -20,7 +19,7 @@ int xe_reg_sr_init(struct xe_reg_sr *sr, const char *name, struct xe_device *xe)
 int xe_reg_sr_dump_kv(struct xe_reg_sr *sr,
 		      struct xe_reg_sr_kv **dst);
 
-int xe_reg_sr_add(struct xe_reg_sr *sr, i915_reg_t reg,
+int xe_reg_sr_add(struct xe_reg_sr *sr, u32 reg,
 		  const struct xe_reg_sr_entry *e);
 void xe_reg_sr_apply_mmio(struct xe_reg_sr *sr, struct xe_gt *gt);
 
