ecaad803ee6eeb0c066ae3bd44a6e46897116fc2
Matthew Brost <matthew.brost@intel.com>
2023-01-24 16:33:09 -0800

    drm/xe: Add has_range_tlb_invalidation device attribute
    This will help implementing range based TLB invalidations.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>

--- drivers/gpu/drm/xe/xe_device_types.h
+++ drivers/gpu/drm/xe/xe_device_types.h
@@ -92,6 +92,8 @@ struct xe_device {
 		bool has_flat_ccs;
 		/** @has_4tile: Whether tile-4 tiling is supported */
 		bool has_4tile;
+		/** @has_range_tlb_invalidation: Has range based TLB invalidations */
+		bool has_range_tlb_invalidation;
 
 		struct xe_device_display_info {
 			u8 ver;
