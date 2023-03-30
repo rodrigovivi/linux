1d6d6e0cfcf216a4a4b4003349dbeeedd4d198c8
Rodrigo Vivi <rodrigo.vivi@intel.com>
2023-03-30 17:31:57 -0400

    drm/xe: Introduce a new DRM driver for Intel GPUs
    Xe, is a new driver for Intel GPUs that supports both integrated and
    discrete platforms starting with Tiger Lake (first Intel Xe Architecture).
    
    The code is at a stage where it is already functional and has experimental
    support for multiple platforms starting from Tiger Lake, with initial
    support implemented in Mesa (for Iris and Anv, our OpenGL and Vulkan
    drivers), as well as in NEO (for OpenCL and Level0).
    
    The new Xe driver leverages a lot from i915.
    
    As for display, the intent is to share the display code with the i915
    driver so that there is maximum reuse there. But it is not added
    in this patch.
    
    This initial work is a collaboration of many people and unfortunately
    the big squashed patch won't fully honor the proper credits. But let's
    get some git quick stats so we can at least try to preserve some of the
    credits:
    
    Co-developed-by: Matthew Brost <matthew.brost@intel.com>
    Co-developed-by: Matthew Auld <matthew.auld@intel.com>
    Co-developed-by: Matt Roper <matthew.d.roper@intel.com>
    Co-developed-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Co-developed-by: Francois Dugast <francois.dugast@intel.com>
    Co-developed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Co-developed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Co-developed-by: Philippe Lecluse <philippe.lecluse@intel.com>
    Co-developed-by: Nirmoy Das <nirmoy.das@intel.com>
    Co-developed-by: Jani Nikula <jani.nikula@intel.com>
    Co-developed-by: José Roberto de Souza <jose.souza@intel.com>
    Co-developed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Co-developed-by: Dave Airlie <airlied@redhat.com>
    Co-developed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Co-developed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Co-developed-by: Mauro Carvalho Chehab <mchehab@kernel.org>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>

---
+++ drivers/gpu/drm/xe/xe_reg_sr_types.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2022 Intel Corporation
+ */
+
+#ifndef _XE_REG_SR_TYPES_
+#define _XE_REG_SR_TYPES_
+
+#include <linux/xarray.h>
+#include <linux/types.h>
+
+#include "i915_reg_defs.h"
+
+struct xe_reg_sr_entry {
+	u32		clr_bits;
+	u32		set_bits;
+	/* Mask for bits to consider when reading value back */
+	u32		read_mask;
+	/*
+	 * "Masked registers" are marked in spec as register with the upper 16
+	 * bits as a mask for the bits that is being updated on the lower 16
+	 * bits when writing to it.
+	 */
+	bool		masked_reg;
+};
+
+struct xe_reg_sr_kv {
+	i915_reg_t		k;
+	struct xe_reg_sr_entry	v;
+};
+
+struct xe_reg_sr {
+	struct {
+		struct xe_reg_sr_entry *arr;
+		unsigned int used;
+		unsigned int allocated;
+		unsigned int grow_step;
+	} pool;
+	struct xarray xa;
+	const char *name;
+};
+
+#endif
