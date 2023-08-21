e1d816044a2feb4aab2592c840e999bd9a13f0a8
Jani Nikula <jani.nikula@intel.com>
2023-08-21 15:54:02 +0300

    FIXME: drm/i915: xe doesn't have fastboot module parameter
    Should be abstracted better.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -4984,6 +4984,7 @@ pipe_config_mismatch(bool fastset, const struct intel_crtc *crtc,
 
 static bool fastboot_enabled(struct drm_i915_private *dev_priv)
 {
+#ifdef I915
 	if (dev_priv->params.fastboot != -1)
 		return dev_priv->params.fastboot;
 
@@ -4997,6 +4998,9 @@ static bool fastboot_enabled(struct drm_i915_private *dev_priv)
 
 	/* Disabled by default on all others */
 	return false;
+#else
+	return true;
+#endif
 }
 
 bool
