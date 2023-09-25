7de4624c9efce6d6608da1b92149ec00e450f732
Francois Dugast <francois.dugast@intel.com>
2023-09-25 11:31:30 +0200

    drm/xe: Extend uAPI to query HuC micro-controler firmware version
    The infrastructure to query GuC firmware version is already in place. It
    is extended with a new micro-controller type to query the HuC firmware
    version. It can be used from user space to know if HuC is running.
    
    Signed-off-by: Francois Dugast <francois.dugast@intel.com>

--- include/uapi/drm/xe_drm.h
+++ include/uapi/drm/xe_drm.h
@@ -475,6 +475,7 @@ struct drm_xe_query_topology_mask {
 struct drm_xe_query_uc_fw_version {
 	/** @uc: The micro-controller type to query firmware version */
 #define XE_QUERY_UC_TYPE_GUC 0
+#define XE_QUERY_UC_TYPE_HUC 1
 	__u16 uc_type;
 
 	/** @pad: MBZ */
