4c48abdcd784d5ed7f5cbb9748bf4d649d02d4f3
Jouni Högander <jouni.hogander@intel.com>
2023-11-06 08:48:27 +0200

    fixup! drm/xe/display: Implement display support
    Using i915_fence_timeout causes linking error as i915_fence_context_timeout
    doesn't exist for Xe. Return just MAX_SCHEDULE_TIMEOUT.
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>
    Acked-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/xe/compat-i915-headers/i915_config.h
+++ drivers/gpu/drm/xe/compat-i915-headers/i915_config.h
@@ -6,18 +6,14 @@
 #ifndef __I915_CONFIG_H__
 #define __I915_CONFIG_H__
 
-#include <linux/types.h>
-#include <linux/limits.h>
+#include <linux/sched.h>
 
 struct drm_i915_private;
-
-unsigned long i915_fence_context_timeout(const struct drm_i915_private *i915,
-					 u64 context);
 
 static inline unsigned long
 i915_fence_timeout(const struct drm_i915_private *i915)
 {
-	return i915_fence_context_timeout(i915, U64_MAX);
+	return MAX_SCHEDULE_TIMEOUT;
 }
 
 #endif /* __I915_CONFIG_H__ */
