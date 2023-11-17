a9f6f02d92c983644be83451a618f5427532f2b8
Lucas De Marchi <lucas.demarchi@intel.com>
2023-11-17 09:56:24 -0800

    fixup! drm/xe/display: Create a dummy version for vga decode
    Fix build with W=1:
    
    	  CC [M]  drivers/gpu/drm/xe/display/intel_fbdev_fb.o
    	../drivers/gpu/drm/xe/display/xe_display_misc.c:10:14: error: no previous prototype for ‘intel_gmch_vga_set_decode’ [-Werror=missing-prototypes]
    	   10 | unsigned int intel_gmch_vga_set_decode(struct pci_dev *pdev, bool enable_decode)
    	      |              ^~~~~~~~~~~~~~~~~~~~~~~~~
    	cc1: all warnings being treated as errors
    
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Link: https://lore.kernel.org/r/20231117175624.535520-3-lucas.demarchi@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- drivers/gpu/drm/xe/display/xe_display_misc.c
+++ drivers/gpu/drm/xe/display/xe_display_misc.c
@@ -7,6 +7,8 @@
 
 struct pci_dev;
 
+unsigned int intel_gmch_vga_set_decode(struct pci_dev *pdev, bool enable_decode);
+
 unsigned int intel_gmch_vga_set_decode(struct pci_dev *pdev, bool enable_decode)
 {
 	/* ToDo: Implement the actual handling of vga decode */
