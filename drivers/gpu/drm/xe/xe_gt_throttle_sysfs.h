4505513de2a2b89e24d623a18db30f5ee38920cd
Sujaritha Sundaresan <sujaritha.sundaresan@intel.com>
2023-12-05 15:08:09 +0530

    drm/xe: Add frequency throttle reasons sysfs attributes
    Add throttle reasons sysfs attributes under a separate directory.
    
    /device/tile<n>/gt<n>/freq0/throttle
    			|- reason_pl1
    			|- reason_pl2
    			|- reason_pl4
    			|- reason_prochot
    			|- reason_ratl
    			|- reason_vr_tdc
    			|- reason_vr_thermalert
    			|- status
    
    v2: Remove unnecessary headers and clean-up action (Riana)
    
    v3: - Add missing 'tile#/' to the doc (Riana)
        - Add missing documentation (Riana)
        - Remove extra line in header (Riana)
        - Remove extra header inclusion missed on v2 (Rodrigo).
    
    Signed-off-by: Sujaritha Sundaresan <sujaritha.sundaresan@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

---
+++ drivers/gpu/drm/xe/xe_gt_throttle_sysfs.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#ifndef _XE_GT_THROTTLE_SYSFS_H_
+#define _XE_GT_THROTTLE_SYSFS_H_
+
+struct xe_gt;
+
+void xe_gt_throttle_sysfs_init(struct xe_gt *gt);
+
+#endif /* _XE_GT_THROTTLE_SYSFS_H_ */
