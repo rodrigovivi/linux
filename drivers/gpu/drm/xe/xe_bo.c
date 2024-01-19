e29200e33965baf51ae81524ed3f023fa9d0503f
Rodrigo Vivi <rodrigo.vivi@intel.com>
2024-01-19 15:54:02 -0500

    drm/xe: Remove inner mem_access protections
    At this point, all the outer bounds callers of direct
    xe_pm_runtime_{get,put}, should already be ensuring that
    device is in D0 at these points of BO creation and
    manipulation. So, time to remove the very last users
    of the xe_device_mem_access_{get,put}.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_bo.c
+++ drivers/gpu/drm/xe/xe_bo.c
@@ -730,7 +730,6 @@ static int xe_bo_move(struct ttm_buffer_object *ttm_bo, bool evict,
 	xe_assert(xe, migrate);
 
 	trace_xe_bo_move(bo);
-	xe_device_mem_access_get(xe);
 
 	if (xe_bo_is_pinned(bo) && !xe_bo_is_user(bo)) {
 		/*
@@ -754,7 +753,6 @@ static int xe_bo_move(struct ttm_buffer_object *ttm_bo, bool evict,
 
 				if (XE_WARN_ON(new_mem->start == XE_BO_INVALID_OFFSET)) {
 					ret = -EINVAL;
-					xe_device_mem_access_put(xe);
 					goto out;
 				}
 
@@ -772,7 +770,6 @@ static int xe_bo_move(struct ttm_buffer_object *ttm_bo, bool evict,
 						new_mem, handle_system_ccs);
 		if (IS_ERR(fence)) {
 			ret = PTR_ERR(fence);
-			xe_device_mem_access_put(xe);
 			goto out;
 		}
 		if (!move_lacks_source) {
@@ -796,8 +793,6 @@ static int xe_bo_move(struct ttm_buffer_object *ttm_bo, bool evict,
 
 		dma_fence_put(fence);
 	}
-
-	xe_device_mem_access_put(xe);
 
 out:
 	return ret;
