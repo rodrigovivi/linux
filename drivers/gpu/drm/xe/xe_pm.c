e9e06138c0bc76be1a24daec037a8c7985727d8a
Badal Nilawar <badal.nilawar@intel.com>
2025-07-08 00:42:35 +0530

    drm/xe/xe_late_bind_fw: Reload late binding fw during system resume
    Reload late binding fw during resume from system suspend
    
    v2:
      - Unconditionally reload late binding fw (Rodrigo)
      - Flush worker during system suspend
    
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Badal Nilawar <badal.nilawar@intel.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_pm.c
+++ drivers/gpu/drm/xe/xe_pm.c
@@ -127,6 +127,8 @@ int xe_pm_suspend(struct xe_device *xe)
 	if (err)
 		goto err;
 
+	xe_late_bind_wait_for_worker_completion(&xe->late_bind);
+
 	for_each_gt(gt, xe, id)
 		xe_gt_suspend_prepare(gt);
 
@@ -203,6 +205,8 @@ int xe_pm_resume(struct xe_device *xe)
 		goto err;
 
 	xe_pxp_pm_resume(xe->pxp);
+
+	xe_late_bind_fw_load(&xe->late_bind);
 
 	drm_dbg(&xe->drm, "Device resumed\n");
 	return 0;
